ebb945182d534a079af64bbbc1b3bba7747d2a6d
ZhengXuJiang <zhengxujiang@jiayouxueba.cn>
2020-05-15 10:14:04 +0800

    chore: 更新配置

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,10 +3,17 @@ module.exports = {
     'postcss-import': {},
     'postcss-url': {},
     'postcss-preset-env': {},
-    cssnano: {
-      preset: 'advanced',
-      autoprefixer: false,
-      'postcss-zindex': false,
+    'postcss-plugin-px2rem': {
+      rootValue: 150,
+      selectorBlackList: [/^html$/, /^body$/],
+      exclude: /(node_module)/,
+      mediaQuery: false,
+      minPixelValue: 2,
     },
+    // cssnano: {
+    //   preset: 'advanced',
+    //   autoprefixer: false,
+    //   'postcss-zindex': false,
+    // },
   },
 };
